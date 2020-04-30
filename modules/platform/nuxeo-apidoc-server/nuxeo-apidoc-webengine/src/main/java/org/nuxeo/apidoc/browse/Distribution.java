c995f247c5da1a4d6504fc1f78a2ded0049e99ef
Anahide Tchertchian <at@nuxeo.com>
2020-04-30 13:14:43 +0200

    NXP-29050: fix explorer property retrieval (regression introduced by 77634a1aa8bd4d348a5505fd376f1cdb836c1c82 with NXP-28839)

--- modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-webengine/src/main/java/org/nuxeo/apidoc/browse/Distribution.java
+++ modules/platform/nuxeo-apidoc-server/nuxeo-apidoc-webengine/src/main/java/org/nuxeo/apidoc/browse/Distribution.java
@@ -529,7 +529,7 @@ public class Distribution extends ModuleRoot {
     }
 
     public static boolean showCurrentDistribution() {
-        return !(Framework.isBooleanPropertyTrue(ApiBrowserConstants.PROPERTY_SITE_MODE) || isSiteMode());
+        return !(Framework.isBooleanPropertyTrue(ApiBrowserConstants.PROPERTY_HIDE_CURRENT_DISTRIBUTION) || isSiteMode());
     }
 
     public static boolean isSiteMode() {
